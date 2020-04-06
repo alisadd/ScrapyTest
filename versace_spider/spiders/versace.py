dc0906fcea4e639027338a5bf3670934d549b433
Alisa Demina <alisa.demina@protonmail.com>
2020-04-06 18:40:25 +0300

    added sku code and men/jeans/children categories

--- versace_spider/spiders/versace.py
+++ versace_spider/spiders/versace.py
@@ -7,7 +7,8 @@ import re
 class VersaceSpider(Spider):
     name = 'versace'
     allowed_domains = ['versace.com']
-    start_urls = ['https://www.versace.com/']
+#    start_urls = ['https://www.versace.com/']
+    start_urls = ['https://www.versace.com/fr/fr-fr/home/']
     custom_settings = {'REDIRECT_ENABLED': False}
 #    handle_httpstatus_list = [302]
     
@@ -15,14 +16,29 @@ class VersaceSpider(Spider):
         '''
         Start crawler, define product categories & search for regional urls
         '''
-        self.region_code = 'us/en-us'
+        self.region_code = 'fr/fr-fr'
         women_category = response.xpath('//li[a/@data-link_description="Women"]//li[@class="level-2-item js-expand-menu "]//a[@class="level-3-link category-link"]')
         for item in women_category:
-            women_category_title = item.xpath('text()').extract_first().strip()
             women_category_url = item.xpath('@href').extract_first()
             women_category_url = response.urljoin(women_category_url)
-            category_url = women_category_url.replace("international/en", self.region_code)
-            yield Request(category_url, callback = self.parse_categories)
+           # category_url = women_category_url.replace("international/en", self.region_code)
+           # yield Request(women_category_url, callback = self.parse_categories)
+        men_category = response.xpath('//li[a/@data-link_description="Men"]//li[@class="level-2-item js-expand-menu "]//a[@class="level-3-link category-link"]')
+        for item in men_category:
+            men_category_url = item.xpath('@href').extract_first()
+            men_category_url = response.urljoin(men_category_url)
+           # yield Request(men_category_url, callback = self.parse_categories)
+        jeans_category = response.xpath('//li[a/@data-link_description="Jeans Couture"]//li[@class="level-2-item js-expand-menu "]//a[@class="level-3-link category-link"]')
+        for item in jeans_category:
+            jeans_category_url = item.xpath('@href').extract_first()
+            jeans_category_url = response.urljoin(jeans_category_url)
+           # yield Request(jeans_category_url, callback = self.parse_categories)
+        children_panel = response.xpath('//li[contains(@class, "children-category")]')[0]
+        children_category = children_panel.xpath('.//a[@class="level-2-link category-link"]')
+        for item in children_category:
+            children_category_url = item.xpath('@href').extract_first()
+            children_category_url = response.urljoin(children_category_url)
+            yield Request(children_category_url, callback = self.parse_categories)
 
 
     def parse_categories(self, response):
@@ -49,7 +65,13 @@ class VersaceSpider(Spider):
         Extract the data from the product pages
         '''
         product_name = response.xpath('//h1[@itemprop="name"]/text()').extract_first()
-        price = response.xpath('//span[@class="js-sl-price"]/text()').extract_first().strip()
+        try:
+            price = response.xpath('//span[@class="js-sl-price"]/text()').extract_first().strip()
+        except AttributeError:
+            price = []
+            price_range = response.xpath('//div[@class="price-range"]//span[contains(@itemprop, "Price")]/text()').extract()
+            for item in price_range[:2]:
+                price.append(item.strip())
         currency = response.xpath('//div[@itemprop="priceCurrency"]/@content').extract_first()
         color = response.xpath('//ul[@class="product-variations-list menu baseline-medium"]//a[contains(@class,"swatchanchor")]/@title').extract()
         size_block = response.xpath('//span[@class="js-swatch-value swatch-value"]/text()').extract()
@@ -66,6 +88,8 @@ class VersaceSpider(Spider):
         time = datetime.now()
         region = self.region_code[:2]
         category = url.split('/')[5:8] 
+        sku_block = response.xpath('//script[contains(text(), "window.universal_variable.product")]').extract_first() 
+        sku = re.search('sku":\"(.*?)\"', sku_block).group(1)
 
         return {"product name": product_name,
                 "color": color,
@@ -76,7 +100,8 @@ class VersaceSpider(Spider):
                 "currency": currency,
                 "size": sizes,
                 "region": region,
-                "category": category} 
+                "category": category,
+                "sku": sku} 
 
 
 
